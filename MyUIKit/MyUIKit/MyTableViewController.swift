3c07153a18c2d5f5f377faa066bdc04e0becf347
chaenchaen99 <chaenjung.99@gmail.com>
2024-01-12 17:47:42 +0900

    wc) Table View Delegate 공부

---
+++ MyUIKit/MyUIKit/MyTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MyTableViewController.swift
+//  MyUIKit
+//
+//  Created by admin on 2024/01/12.
+//
+
+import UIKit
+
+class MyTableViewController: UIViewController {
+
+    @IBOutlet var myTableView: UITableView!
+    
+    let cellData = ["Hello TableView!","This is UIKit","Welcome!","My name is","Chaeyeon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        myTableView.backgroundColor = .green
+        myTableView.delegate = self
+        myTableView.dataSource = self
+    }
+}
+
+extension MyTableViewController:
+    UITableViewDelegate,
+    UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = cellData[indexPath.row]
+        return cell
+    }
+    
+    
+}
