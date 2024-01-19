e0d7d3edf4eba97a6967346de9b405dc8dbc2255
chaenchaen99 <chaenjung.99@gmail.com>
2024-01-19 18:20:19 +0900

    wc) 음성메모 앱 프로젝트 및 readme 생성

---
+++ VoiceRecoder/VoiceRecoder/VoiceRecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecoderApp.swift
+//  VoiceRecoder
+//
+//  Created by admin on 2024/01/19.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
