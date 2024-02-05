9110e6fcd6a6fc5064072d882c26b72ca620a53e
chaeyeon <chaenjung.99@gmail.com>
2024-02-05 22:17:19 +0900

    wc) todo list 뷰 구현중

--- ios_study/IOS/BASIC/voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
+++ ios_study/IOS/BASIC/voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
@@ -6,15 +6,50 @@
 import SwiftUI
 
 struct TodoListView: View {
-    @EnvironmentObject private pathModel: PathModel
+    @EnvironmentObject private var pathModel: PathModel
+    @EnvironmentObject private var todolistViewModel: TodoListViewModel
     
   var body: some View {
-    Text("Todo List")
+      ZStack {
+          //투두 셀 리스트
+          VStack{
+              if !todolistViewModel.todos.isEmpty {
+                  CustomNavigationBar(
+                    isDisplayLeftBtn: false,
+                    rightBtnAction: {
+                        todolistViewModel.navigationRightBtnTapped()
+                    },
+                    rightBtnType: todolistViewModel.navigationBarRightBtnMode
+                  )
+              } else {
+                  Spacer()
+                      .frame(height: 30)
+              }
+              TitleView()
+          }
+      }
   }
+}
+private struct TitleView: View {
+    @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    
+    fileprivate var body: some View {
+        HStack {
+            if todoListViewModel.todos.isEmpty {
+                Text("To do list를\n추가해보세요")
+            }else{
+                Text("To do list \(todoListViewModel.todos.count)개가\n있습니다.")
+            }
+            Spacer()
+        }.font(.system(size: 30, weight: .bold))
+            .padding(.leading, 20)
+    }
 }
 
 struct TodoListView_Previews: PreviewProvider {
   static var previews: some View {
     TodoListView()
+          .environmentObject(PathModel())
+          .environmentObject(TodoListViewModel())
   }
 }
