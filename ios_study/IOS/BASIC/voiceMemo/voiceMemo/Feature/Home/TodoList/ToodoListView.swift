c245235fe535a2bf6b76f06c55f92a01604b278c
chaeyeon <chaenjung.99@gmail.com>
2024-02-06 21:58:31 +0900

    wc) todolist 구현

--- ios_study/IOS/BASIC/voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
+++ ios_study/IOS/BASIC/voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
@@ -26,6 +26,13 @@ struct TodoListView: View {
                       .frame(height: 30)
               }
               TitleView()
+                  .padding(.top, 20)
+              if todolistViewModel.todos.isEmpty {
+                  AnnouncementView()
+              }else{
+                  TodoListContentView()
+                      .padding(.top, 20)
+              }
           }
       }
   }
@@ -43,6 +50,108 @@ private struct TitleView: View {
             Spacer()
         }.font(.system(size: 30, weight: .bold))
             .padding(.leading, 20)
+    }
+}
+
+private struct AnnouncementView: View {
+    fileprivate var body: some View {
+        VStack (spacing: 15){
+            Spacer()
+            Image("pencil")
+                .renderingMode(.template)
+            Text("\"매일 아침 5시 운동하자!\"")
+            Text("\"내일 8시 수강신청하라고 알려줘\"")
+            Text("\"1시 반 점심 약속 리마인드 해줘\"")
+            
+            Spacer()
+        }
+        .font(.system(size: 16))
+        .foregroundColor(.customGray2)
+    }
+}
+
+//MARK: - TodoList 컨텐츠 뷰
+private struct TodoListContentView: View {
+    @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    
+    fileprivate var body: some View {
+        VStack{
+            HStack{
+                Text("할일 목록")
+                    .font(.system(size: 16, weight: .bold))
+                    .padding(.leading, 20)
+                
+                Spacer()
+            }
+            ScrollView(.vertical){
+                VStack(spacing: 0)
+                {
+                    Rectangle()
+                        .fill(Color.customGray0)
+                        .frame(height: 1)
+                    
+                    ForEach(todoListViewModel.todos, id: \.self) { todo in
+                        //TODO: todo셀 뷰 todo 넣어서 뷰 호출
+                        TodoCellView(todo: todo)
+                    }
+                }
+            }
+        }
+    }
+}
+//MARK: todo 셀 뷰
+private struct TodoCellView: View {
+    @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    @State private var isRemoveSelected: Bool
+    private var todo: Todo
+    
+    fileprivate init(
+        isRemoveSelected: Bool = false,
+        todo: Todo
+    ){
+        _isRemoveSelected = State(initialValue: isRemoveSelected)
+        self.todo = todo
+    }
+    
+    fileprivate var body: some View {
+        VStack(spacing: 20) {
+            HStack {
+                if !todoListViewModel.isEditTodoMode {
+                    Button(
+                        action: { todoListViewModel.selectedBoxTapped(todo)},
+                        label: {todo.selected ? Image("selectedBox"): Image("unSelectedBox")}
+                    )
+                }
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(todo.title)
+                        .font(.system(size: 16))
+                        .foregroundColor(todo.selected ? .customIconGray : .customBlack)
+                    
+                    Text(todo.convertedDayAndTime)
+                        .font(.system(size: 16))
+                        .foregroundStyle(.customIconKey)
+                }
+                Spacer()
+                
+                if todoListViewModel.isEditTodoMode {
+                    Button(
+                        action: {
+                            isRemoveSelected.toggle()
+                            todoListViewModel.todoRemoveSelectedBoxTapped(todo)
+                        },
+                        label: {
+                            isRemoveSelected ? Image("selectedBox") : Image("unSelectedBox")
+                        }
+                    )
+                }
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 10)
+            
+            Rectangle()
+                .fill(Color.customGray0)
+                .frame(height: 1)
+        }
     }
 }
 
